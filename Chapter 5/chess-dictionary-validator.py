d0bb01fb8986846e1bba4e312e92a0b1d6333fd9
austinare <austinjrichards@outlook.com>
2021-02-08 17:53:44 -0700

    Finished check location functionality

--- Chapter 5/chess-dictionary-validator.py
+++ Chapter 5/chess-dictionary-validator.py
@@ -4,18 +4,28 @@ Austin Richards 2/8/21
 Chess dictionary validator will take a dictionary with keys and values in the form
 'location':'piece_type' and check wether it is a valid board
 (wether there are the correct amount of pieces, they are placed on the board, etc.)
-'''
 
-layout = {'1h': 'bking', '6c': 'wqueen', '2g': 'bbishop', '5h': 'bqueen', '3e': 'wking'}
-
-'''
 Valid board criteria:
-- each player can only have 16 pieces at most
-    - at most 8 pawns, 1 queen, and 2 of the rest
-- each piece exists on the board
--BONUS: make the dictionary able to detect if bishops are duplicates based on board location
+- each player can only have 16 pieces at most (at most 8 pawns, 1 queen, and 2 of the rest)
+- each piece exists on the board and is not in the same place as any other pieces
+- BONUS: make the dictionary able to detect if bishops are duplicates based on board location
 '''
 
-# check that pieces are in a valid location, and that there are no repeats
+valid_board = True
+board = {'1h': 'bking', '6c': 'wqueen', '2g': 'bbishop', '5h': 'bqueen', '3e': 'wking'}
 
-# check that there are the correct number of each piece (ex. only two queens, one white one black)
+locs   = list(board.keys())
+pieces = list(board.values())
+
+for pos in locs:
+    
+    if int(pos[0]) > 8 or int(pos[0]) < 1:
+        valid_board = False
+
+    if pos[1] > 'h':
+        valid_board = False
+
+for piece_type in pieces:
+    print(piece_type)
+
+print(valid_board)
