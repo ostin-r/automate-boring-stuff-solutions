aa771e4ec941f33b6a192ab181c6b1d9d530a9a5
austinare <austinjrichards@outlook.com>
2021-02-10 17:43:54 -0700

    Added bishop check feature

--- Chapter 5/chess-dictionary-validator.py
+++ Chapter 5/chess-dictionary-validator.py
@@ -11,10 +11,11 @@ Valid board criteria:
 - BONUS: make the dictionary able to detect if bishops are duplicates based on board location
 '''
 
-board = {'1h': 'bking', '6c': 'wqueen', '2g': 'wbishop', '5h': 'bqueen', '3e': 'wking', '2f':'wbishop'}
+board = {'1h': 'bking', '6c': 'wqueen', '2g': 'wbishop', '5h': 'bqueen', '3e': 'wking', '2f':'wbishop', '6a':'bbishop', '6c':'bbishop'}
 
 locs   = list(board.keys())
 pieces = list(board.values())
+board_list = list(board.items())
 
 valid_board = 'bking' and 'wking' in board.values()
 
@@ -67,13 +68,36 @@ for i in range(1, len(x), 2):
 
 if piece_count.get('wbishop', 0) > 1:
 
-    position = []
+    wbishop_locs = []
 
-    for piece in pieces:
+    for loc, piece in board_list:
 
         if piece == 'wbishop':
 
-            position.append(pieces.index(piece))
-            
+            wbishop_locs.append(loc)
 
+    if wbishop_locs[0] in black_squares and wbishop_locs[1] in black_sqaures:
+        valid_board = False
+
+    if wbishop_locs[0] not in black_squares and wbishop_locs[1] not in black_squares:
+        valid_board = False
+
+if piece_count.get('bbishop', 0) > 1:
+
+    bbishop_locs = []
+
+    for loc, piece in board_list:
+
+        if piece == 'bbishop':
+
+            bbishop_locs.append(loc)
+
+    if bbishop_locs[0] in black_squares and bbishop_locs[1] in black_sqaures:
+        valid_board = False
+
+    if bbishop_locs[0] not in black_squares and bbishop_locs[1] not in black_squares:
+        valid_board = False
+            
+print(bbishop_locs)
+print(wbishop_locs)
 print(valid_board)
