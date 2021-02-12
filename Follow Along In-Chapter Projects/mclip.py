e35119bbe4bc64d64c84dd1c19a96cd1a70e49bf
austinare <austinjrichards@outlook.com>
2021-02-12 14:26:01 -0700

    Getting started on a command line application!

---
+++ Follow Along In-Chapter Projects/mclip.py
@@ -0,0 +1,11 @@
+''' 
+A follow along program in Automate the Boring Stuff
+for replying to emails quickly by using the pyperclip
+module to quickly paste prepared responses
+'''
+import pyperclip
+
+TEXT = {'agree': """Yes, I agree. That sounds fine to me.""",
+        'busy': """Sorry, can we do this later this week or next week?""",
+        'upsell': """Would you consider making this a monthly donation?"""}
+
