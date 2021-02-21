614df4abce06ecf53502b1e0c1e805d20753b65b
austinare <austinjrichards@outlook.com>
2021-02-21 12:08:32 -0700

    completed timeout functionality

--- Chapter 8/minimal-multiply-quiz.py
+++ Chapter 8/minimal-multiply-quiz.py
@@ -3,7 +3,7 @@ Austin Richards 2/21/21
 '''
 import random, time
 
-questionAmount = 3
+questionAmount = 5
 correctAnswers = 0
 
 for questionNumber in range(questionAmount):
@@ -16,14 +16,27 @@ for questionNumber in range(questionAmount):
     print(prompt)
 
     incorrectCount = 0
+    startTime = time.time()
 
     while True:
         try:
             userInput = int(input())
 
+            timeout = time.time() - startTime
+            if timeout > 8.0:
+                if userInput == ans:
+                    print('Correct, but took too long\n')
+                    time.sleep(1)
+                    break
+                else:
+                    print('Incorrect, took too long\n')
+                    time.sleep(1)
+                    break
+
             if userInput == ans:
                 correctAnswers += 1
                 print('Correct\n')
+                time.sleep(1)
                 break
 
             elif userInput != ans and incorrectCount < 2:
@@ -31,8 +44,11 @@ for questionNumber in range(questionAmount):
                 print('Incorrect, try again')
 
             else:
-                print('Incorrect, too many guesses')
+                print('Incorrect, too many guesses\n')
+                time.sleep(1)
                 break
 
         except ValueError:
             print('Please enter a number.')
+
+print('Score: {0} / {1} ({2}%)'.format(correctAnswers, questionAmount, 100*correctAnswers/questionAmount))
