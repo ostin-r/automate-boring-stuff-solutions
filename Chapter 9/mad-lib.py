47abf9701fe6b03681186fe04499cad26a847584
austinare <austinjrichards@outlook.com>
2021-02-26 05:30:51 -0700

    got parts of speech regex working

--- Chapter 9/mad-lib.py
+++ Chapter 9/mad-lib.py
@@ -10,6 +10,7 @@ from pathlib import Path
 
 madLib_blank = open(Path.cwd() / 'Chapter 9' / 'mad-lib-blank.txt')
 blank_contents = madLib_blank.read()
+print(blank_contents)
 
 # TODO: use regex (loop) to identify the next word that needs to be replaced and update the contents
 '''
@@ -18,12 +19,24 @@ put that list into a regex
 loop through all items in re.findall()
     if item == 'ADJECTIVE'
         prompt user to enter an adjective
-        replace that item in the updated text with re.sub()
+        replace in new string with re.sub()
     elif item == 'VERB':
         prompt user to enter a verb
-        replace that item in the updated text re.sub()
-        ...
-    
+        replace in new string using re.sub()
 '''
+keyword_regex = re.compile(r'''
+    ADJECTIVE|
+    VERB|
+    ADVERB|
+    PRONOUN|
+    INTERJECTION|
+    NOUN|
+    ''', re.VERBOSE)
+
+mo = keyword_regex.search(blank_contents)
+try:
+    print(mo.group())
+except:
+    print('no match')
 
 # TODO: after looping through all blank word types, write the newly filled contents to a new .txt file
