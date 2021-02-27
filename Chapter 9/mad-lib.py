690066de788bd1336fff9a8c12ebab2f9ff01f84
austinare <austinjrichards@outlook.com>
2021-02-27 07:20:06 -0700

    completed text file writing

--- Chapter 9/mad-lib.py
+++ Chapter 9/mad-lib.py
@@ -24,5 +24,15 @@ for word in keyword_regex.findall(contents):
     user_input = pyip.inputStr(prompt=statement)
     contents   = re.sub(word, user_input, contents, count=1)
 
-# TODO: after looping through all blank word types, write the newly filled contents to a new .txt file
-madLib = open('madLib-1.txt', 'w')
+i = 1
+while True:
+    fileName = 'madLib-{}.txt'.format(i)
+    filePath = Path.cwd() / 'Chapter 9' / fileName
+
+    if filePath.exists():
+        i += 1
+
+    else:
+        newFile = open(filePath, 'w')
+        newFile.write(contents)
+        break
