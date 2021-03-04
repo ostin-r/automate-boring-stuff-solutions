fe96e16f54f97562a9140cd8be7e7ee9ba40d74a
austinare <austinjrichards@outlook.com>
2021-03-04 05:32:11 -0700

    completed changing filenames, need to actually move them now

---
+++ Chapter 10/del-numbspace.py
@@ -0,0 +1,34 @@
+'''
+Austin Richards 3/4/21
+
+del-numbspace.py will look for leading zeros in filenames of
+the format text009.py and rename the file without the zeros
+'''
+import os, re, shutil
+
+def get_all_paths(directory, abs_path):
+
+    file_paths = []
+
+    if abs_path:
+        directory = os.path.abspath(directory)
+
+    for path, dirs, files in os.walk(directory):
+
+        for filename in files:
+            filepath = os.path.join(path, filename)
+            file_paths.append(filepath)
+
+    return file_paths
+
+filepath = get_all_paths('Chapter 10', False)
+zero_regex = re.compile(r'[a-zA-Z]+(0+)\d+.*')
+
+for file in filepath:
+
+    mo = zero_regex.search(file)
+
+    if mo is not None:
+        new_name = re.sub(mo.group(1), '', file)
+        #shutil.move()
+        print(new_name)
