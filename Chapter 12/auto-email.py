79ccd73a2c4573077e0891d52ad142346618acca
austinare <austinjrichards@outlook.com>
2021-03-09 05:31:01 -0700

    To do list for auto emailer

---
+++ Chapter 12/auto-email.py
@@ -0,0 +1,12 @@
+'''
+Austin Richards 3/9/21
+
+auto-email.py is a basic emailer that sends emails via the selenium python module.
+'''
+import selenium, pyinputplus
+
+# TODO: Open a webbrowser, navigate to the login page
+
+# TODO: Get the user's credentials (remember to use pyip for password)
+
+# TODO: Create + send an email
