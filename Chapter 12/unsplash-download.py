2d1c417b733d617e9114251b5de1973622374317
austinare <austinjrichards@outlook.com>
2021-03-18 16:27:19 -0600

    downloading function working, updated function description

--- Chapter 12/unsplash-download.py
+++ Chapter 12/unsplash-download.py
@@ -1,28 +1,44 @@
 '''
 Austin Richards 3/11/21
 
-flickr-download will search flickr
-and download the first 10 images
+unsplash-download will search unsplash.com, an
+image sharing website and download the first 3 images
 '''
-import os, requests, bs4, re
+import os, requests, bs4
 import logging as log
 from contextlib import suppress
 
 log.basicConfig(level=log.DEBUG, format='%(asctime)s: %(message)s')
+log.disable(log.CRITICAL)
 
 search_term = 'canyon'
 url = 'https://unsplash.com/s/photos/' + search_term
+new_directory = 'unsplash-' + search_term
+
 os.chdir('Chapter 12')
-os.makedirs('unsplash-' + search_term, exist_ok=True)
+os.makedirs(new_directory, exist_ok=True)
 
 res = requests.get(url)
 res.raise_for_status()
 soup = bs4.BeautifulSoup(res.text, 'html.parser')
 links = soup.select('img')
 
-for link in links:
+i = 1
+for link in links[0:4]:
     with suppress(KeyError):
-        log.debug( '\nDownloading image link: ' + link['srcset'] + '\n' )
 
-# TODO: Make a regex object that can sort through these massive links
-# TODO: maybe they can be indexed but I couldn't figure it out
+        if link['src'].startswith('https://images'):
+            log.debug('downloading... ' + link['src'][:70])
+            res = requests.get(link['src'])
+            res.raise_for_status()
+
+            filename = 'image-' + str(i) + '.jpg'
+            filepath = os.path.join(new_directory, filename)
+            log.debug(filepath)
+
+            with open(filepath, 'wb') as file:
+                for chunk in res.iter_content(100000):
+                    file.write(chunk)
+            
+            print('successfully downloaded image ' + str(i))
+            i += 1
