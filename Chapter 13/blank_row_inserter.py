66451881b9ae94b6d0f3929f35e7e854ef259e8a
austinare <austinjrichards@outlook.com>
2021-03-22 05:34:14 -0600

    updated TODO comments, got loop for copying data setup

--- Chapter 13/blank_row_inserter.py
+++ Chapter 13/blank_row_inserter.py
@@ -1,9 +1,10 @@
 '''
 blank_row_inserter.py takes an excel file and
-inserts 'm' blank rows at row 'n'
+inserts 'm' blank rows at row 'n' (inbetween n and n+1)
 '''
 import os
 import openpyxl as xl
+from openpyxl.utils import get_column_letter, column_index_from_string
 import logging as log
 
 log.basicConfig(level=log.DEBUG, format='%(asctime)s: %(message)s')
@@ -14,16 +15,17 @@ def insert_blanks(file, n, m):
     wb = xl.load_workbook(file)
     sheet = wb.active
 
+    # copy all data down 'm' rows
+    print('copying data...')
     for row in range(1, sheet.max_row):
-        # TODO: insert blanks at n up to n+m (if row > n and row < n + m)
-        # TODO: don't loose the data though, need to copy that over
-        # TODO: figure out how to copy an entire row (can it be done without looping?)
-        pass
-
-    return
+        if row > n:
+            for col in range(0, sheet.max_column):
+                # TODO: copy values down to n+m+1
+    
+    # TODO: insert blank lines at range(n, n+m+1)
 
 os.chdir('Chapter 13')
 
-insert_blanks('produceSales.xlsx', 3, 4)
+insert_blanks('writeFormula.xlsx', 1, 4)
 
 # TODO: Idea: maybe loop from bottom to top in order to preserve data better?
