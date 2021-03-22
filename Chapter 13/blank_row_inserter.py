72b7eefe5d010330379cddc308a89380ad211f8f
austinare <austinjrichards@outlook.com>
2021-03-22 17:23:09 -0600

    finished blank row insert function.  Started to-do list for a function that can move excel functions and maintain relative references

--- Chapter 13/blank_row_inserter.py
+++ Chapter 13/blank_row_inserter.py
@@ -2,7 +2,7 @@
 blank_row_inserter.py takes an excel file and
 inserts 'm' blank rows at row 'n' (inbetween n and n+1)
 '''
-import os
+import os, re
 import openpyxl as xl
 from openpyxl.utils import get_column_letter, column_index_from_string
 import logging as log
@@ -15,17 +15,40 @@ def insert_blanks(file, n, m):
     wb = xl.load_workbook(file)
     sheet = wb.active
 
-    # copy all data down 'm' rows
+    # copy all data down +m rows
     print('copying data...')
-    for row in range(1, sheet.max_row):
-        if row > n:
-            for col in range(0, sheet.max_column):
-                # TODO: copy values down to n+m+1
+    for row in range(sheet.max_row, 1, -1): # decrement from bottom row (no data loss)
+       if row > n:
+            for col in range(1, sheet.max_column + 1):
+                letter = get_column_letter(col)
+                loc = letter + str(row)
+                val = sheet[loc].value
+                
+                new_loc = letter + str(row + m)
+                sheet[new_loc].value = val
+
+                log.debug(f'Copying {val} from {loc} to {new_loc}')
     
-    # TODO: insert blank lines at range(n, n+m+1)
+    print('inserting blank rows...')
+    for row in range(n + 1, n + m + 1):
+        for col in range(1, sheet.max_column + 1):
+            letter = get_column_letter(col)
+            loc = letter + str(row)
+            sheet[loc].value = ''
+            log.debug(f'blank value at {loc}')
+
+    wb.save('insertBlanksExample.xlsx')
+
+def move_excel_function(value, m):
+    '''
+    moves a function excel and maintains relative reference for rows only
+    '''
+    # TODO: match a location using regex ex. 'A1' 'B32', do the following:
+    # get the location of that location in the string (only need the row value)
+    # if a $ preceeds the row don't change it
+    # increase row by m
+    return
+
 
 os.chdir('Chapter 13')
-
-insert_blanks('writeFormula.xlsx', 1, 4)
-
-# TODO: Idea: maybe loop from bottom to top in order to preserve data better?
+insert_blanks('writeFormula.xlsx', 1, 2)
