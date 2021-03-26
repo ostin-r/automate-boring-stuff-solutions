24021a2a9395347169604a61ca9e5c0531356033
austinare <austinjrichards@outlook.com>
2021-03-26 10:00:09 -0600

    finished transposing part of function, can now save transposed version of excel file

--- Chapter 13/cell_transpose.py
+++ Chapter 13/cell_transpose.py
@@ -16,15 +16,28 @@ def transpose_all(file):
     sheet = wb.active
     data = [[] for x in range(sheet.max_column)] # a list of lists to hold column data
 
+    new_rows = sheet.max_column # clear up the confusion of transposing...
+    new_cols = sheet.max_row    # also important to get these numbers before cutting/pasting data
+
+    # 'cut' data into a list
     for col in range(1, sheet.max_column + 1):
         col_letter = get_column_letter(col)
 
         for row in range(1, sheet.max_row + 1):
             current_value = sheet[col_letter + str(row)].value
             sheet[col_letter + str(row)].value = '' # cut
-            data[col - 1].append(current_value)     # paste
+            data[col - 1].append(current_value)     # store in list
 
-    # TODO: put all the data back, with rows and cols flipped
+    # 'paste' in new locations
+    for row in range(1, new_rows + 1):
+        for col in range(1, new_cols + 1):
+            col_letter = get_column_letter(col)
+            new_value = data[row - 1][col - 1]
+            sheet[col_letter + str(row)].value = new_value
+
+    print('Saving file...')
+    wb.save('transcribe-example.xlsx')
+    print('Done!')
 
 
 os.chdir('Chapter 13')
