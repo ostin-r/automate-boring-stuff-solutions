33d448fe724bfa574c8d5bbaa308900a893fcd86
austinare <austinjrichards@outlook.com>
2021-03-29 16:46:26 -0600

    Updated file description.  Completed convert_sheet function.

--- Chapter 14/sheets_convert.py
+++ Chapter 14/sheets_convert.py
@@ -3,7 +3,8 @@ Austin Richards 3/29/21
 
 sheets_convert.py converts different spreadsheet
 files by uploading them to google sheets and then
-downloading them in the correct form
+downloading them in the specified form.  Default is xlsx.
+Valid file conversions are .xlsx, .csv, and .pdf
 '''
 import os
 import logging as log
@@ -13,15 +14,18 @@ from pathlib import Path as path
 log.basicConfig(level=log.DEBUG, format='%(asctime)s : %(message)s')
 
 
-def convert_sheet(file, file_type):
-    #TODO: upload the file to google sheets
+def convert_sheet(file, file_type=None):
+    google_file = sheets.upload(file)
 
-    #TODO: download the file in the desired form
+    if file_type is None or file_type == 'xlsx':
+        google_file.downloadAsExcel()
+    elif file_type == 'csv':
+        google_file.downloadAsCSV()
+    elif file_type == 'pdf':
+        google_file.downloadAsPDF()
 
-    #TODO: delete the file
-
-    return
+    google_file.delete()
 
 
-os.chdir('Chapter 13')
-convert_sheet('files1&2.xlsx', 'xlsx')
+os.chdir('Chapter 14')
+convert_sheet('files1&2.xlsx', 'html')
