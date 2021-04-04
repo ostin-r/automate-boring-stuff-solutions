286a3103716de2e8f06a8b06db71fb104309f72d
austinare <austinjrichards@outlook.com>
2021-04-04 12:26:32 -0600

    completed encrypt_pdfs function, added comments to clear up what each block is doing

--- Chapter 15/pdf_paranoia.py
+++ Chapter 15/pdf_paranoia.py
@@ -9,6 +9,7 @@ and changes the suffix to _decrypted.pdf
 '''
 import os
 import PyPDF2 as PyPDF
+from PyPDF2.utils import PdfReadError
 import logging as log
 from pathlib import Path
 
@@ -30,38 +31,52 @@ def get_all_paths(directory):
 
 
 def encrypt_pdfs(file_path, passcode):
+    # get all the .pdf file paths
     file_path = os.path.abspath(file_path)
     print(f'Going through files in {file_path}')
     files = get_all_paths(file_path)
     files = [file for file in files if file.endswith('.pdf')] # only get pdf files
 
     for file in files:
+        # initialize filename, pdf reader and writer, encrypt the writer
         file_name = Path(file).name
         pdf_file = open(file, 'rb')
         pdf_reader = PyPDF.PdfFileReader(pdf_file)
         pdf_writer = PyPDF.PdfFileWriter()
         pdf_writer.encrypt(passcode)
 
+        # skip files that are already encrypted
         if pdf_reader.isEncrypted:
             print(f'{file_name} already encrypted')
             continue
         
+        # write pages into the newly encrypted writer
         print(f'Encrypting {file_name}...')
         for page in range(pdf_reader.numPages):
             new_page = pdf_reader.getPage(page)
             pdf_writer.addPage(new_page)
 
+        # create a new name and save to a new path
         encrypted_name = Path(file).stem + '_encrypted.pdf'
         new_filepath = os.path.join(Path(file).parent, encrypted_name)
         with open(new_filepath, 'wb') as encrypted_file:
             pdf_writer.write(encrypted_file)
-        print(f'Successfully encrypted {file_name}\n')
 
-        #TODO: close the new file
+        # check that the copy worked, delete the unencrypted file
+        try:
+            encrypted_reader = PyPDF.PdfFileReader(open(new_filepath, 'rb'))
+            encrypted_reader.decrypt(passcode)
+            test_page = encrypted_reader.getPage(0)
+            pdf_file.close()
+            os.unlink(file)
+            print(f'{file_name} encrypted to {Path(new_filepath).name} successfully')
+        except PdfReadError:
+            print('Error: file not encrypted correctly')
 
-        #TODO: attempt to decrypt and read the new file to check that it worked
 
-        #TODO: delete the previous file if the above worked
+def decrypt_pdfs(file_path, passcode):
+    #TODO: get all the .pdf file paths in the path
+    pass
 
 
 encrypt_pdfs('Chapter 15', 'bananas')
