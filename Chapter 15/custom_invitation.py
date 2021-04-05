616e494273d0849360d2c6f55b8b0dbbc349167c
austinare <austinjrichards@outlook.com>
2021-04-05 05:31:53 -0600

    wrote description, todo list for function

---
+++ Chapter 15/custom_invitation.py
@@ -0,0 +1,15 @@
+'''
+Austin Richards 4/5/21
+
+custom_invitation.py creates personalied invitations and puts
+them all on separate pages in a .docx file.  Invitations are
+personalized by reading a .txt file of recipient names.
+'''
+import os
+import docx
+
+#TODO: for each name in the list, do the following:
+
+    #TODO: write the invitation, with the name
+
+    #TODO: add a page break
