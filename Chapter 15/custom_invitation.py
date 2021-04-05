be986961936749a7bcc990421396c9e49f122d6c
austinare <austinjrichards@outlook.com>
2021-04-05 17:53:02 -0600

    Now able to save custom invitation, working on formatting

--- Chapter 15/custom_invitation.py
+++ Chapter 15/custom_invitation.py
@@ -8,8 +8,31 @@ personalized by reading a .txt file of recipient names.
 import os
 import docx
 
-#TODO: for each name in the list, do the following:
 
-    #TODO: write the invitation, with the name
+def custom_invitation(text_file):
+    names_file = open(text_file, 'r')
+    #names = names_file.readlines()
+    names_file.close()
 
-    #TODO: add a page break
+    names = ['Austin']
+
+    for name in names:
+        #TODO: write the invitation, with the name
+        greeting = 'It would be a pleasure to have the company of\n'
+        address = 'at 11010 Memory Lane on the evening of\n'
+        date = 'April 1st\n'
+        time = "at 7 o'clock"
+
+        doc = docx.Document()
+
+        add_greeting = doc.add_paragraph(greeting)
+        add_address = doc.add_paragraph(address)
+        add_date = doc.add_paragraph(date, )
+        add_time = doc.add_paragraph(time, 'Quote')
+        add_time.alignment = 1
+
+        doc.save('invitations.docx')
+
+
+os.chdir('Chapter 15')
+custom_invitation('name_list.txt')
