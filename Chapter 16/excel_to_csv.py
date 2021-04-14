1fc5e155603dbeb0e1bec2797276c12799f3e375
austinare <austinjrichards@outlook.com>
2021-04-14 15:27:07 -0600

    made new file paths to store the new csv files in.  Need to figure out efficient way to save them.

--- Chapter 16/excel_to_csv.py
+++ Chapter 16/excel_to_csv.py
@@ -7,6 +7,7 @@ in the format <filename>_<sheetname>.csv
 '''
 import logging
 import os, csv, openpyxl
+from pathlib import Path
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s: %(message)s')
 
@@ -20,21 +21,23 @@ def get_all_paths(directory):
 
 def xl_to_csv(directory):
     directory = os.path.abspath(directory)
+    new_dir = os.path.join(directory, 'converted_csv_files')
+    os.makedirs(new_dir, exist_ok=True)
     all_paths = [path for path in get_all_paths(directory) if path.endswith('.xlsx')]
-    os.makedirs(f'csvFiles_{os.path.basename(directory)}', exist_ok=True)
-    #TODO put the new directory in the directory being edited, save the name of the new directory
-    # so that it can be referenced later when adding files
     
     for excel_file in all_paths:
         workbook = openpyxl.load_workbook(excel_file)
+        excel_filename = Path(excel_file).stem
 
-        for sheet_name in workbook.get_sheet_names():
-            sheet = workbook.get_sheet_by_name(sheet_name)
-
-            #TODO create the csv file based on the name of the file
+        for sheet_name in workbook.sheetnames:
+            csv_filename = f'{excel_filename}_{sheet_name}.csv'
+            csv_file = os.path.join(new_dir, csv_filename)
+            logging.debug(csv_file)
 
             #TODO loop through every row in the xlsx file and copy it to the csv
+            sheet = workbook[sheet_name]
 
         #TODO: close the new csv file
+
 
 xl_to_csv('Chapter 16')
