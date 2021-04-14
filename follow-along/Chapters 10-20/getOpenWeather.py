1d3251e0670251f6a7c7aa971f724dc5489b5c65
austinare <austinjrichards@outlook.com>
2021-04-13 18:03:37 -0600

    successfully gathered data from open weather API

--- follow-along/Chapters 10-20/getOpenWeather.py
+++ follow-along/Chapters 10-20/getOpenWeather.py
@@ -3,10 +3,15 @@ getOpenWeather.py is a follow along project from Ch. 16
 which downloads json weather data from OpenWeatherMap.org
 and prints it to a text file
 '''
-import json, requests, sys, os
+import json, requests, sys, os, pprint
 import logging as log
 
 log.basicConfig(level=log.DEBUG, format='%(asctime)s: %(message)s')
+log.disable(log.CRITICAL)
+
+def convertKF(temp_K):
+    temp_F = (temp_K - 273.15) * (9/5) + 32
+    return round(temp_F, 2)
 
 if len(sys.argv) < 2:
     print('Usage: python getOpenWeather.py city_name')
@@ -22,3 +27,18 @@ log.debug(location)
 url = f'http://api.openweathermap.org/data/2.5/weather?q={location}&appid={api_key}'
 response = requests.get(url)
 response.raise_for_status()
+
+# change the weather data from json to python dict
+weather_data = json.loads(response.text)
+pprint.pprint(weather_data)
+
+description = weather_data['weather'][0]['description']
+temp = convertKF(weather_data['main']['temp'])
+feels_like = convertKF(weather_data['main']['feels_like'])
+humidity = weather_data['main']['humidity']
+country = weather_data['sys']['country'] 
+
+print(f'\nCurrent weather for {location}, {country}:')
+print(f'Description: {description}')
+print(f'Temperature: {temp} F')
+print(f'Feels Like: {feels_like} F')
