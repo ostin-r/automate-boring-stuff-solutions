d83a554abc115fb1ddba996840108b935726fc38
austinare <austinjrichards@outlook.com>
2021-04-18 14:57:03 -0600

    Timed function to compare to threading project

--- follow-along/Chapters 10-20/xkcd-download.py
+++ follow-along/Chapters 10-20/xkcd-download.py
@@ -2,16 +2,18 @@
 Another follow-along project in ch. 12 (web scraping)
 This project downloads all comics from xkcd.com
 '''
+import time
 import requests, os, bs4
 
 url = 'https://xkcd.com'
 os.chdir('follow-along/Chapters 10-20')
 os.makedirs('xkcd', exist_ok=True)
 
+start = time.time()
 i = 0
 
 #while not url.endswith('#'): # original author's code, but didn't want to download this much
-while i < 3:
+while i <= 40:
     print(f'downloading page {url}...')
     res = requests.get(url)
     res.raise_for_status()
@@ -39,3 +41,6 @@ while i < 3:
     print(url)
     
     i += 1
+
+end = time.time()
+print(f'Done in {end - start} seconds')
