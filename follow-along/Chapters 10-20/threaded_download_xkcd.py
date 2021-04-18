cea8abd5a10e5112f64d7f3d0ef6d0f3bd717c97
austinare <austinjrichards@outlook.com>
2021-04-18 14:21:52 -0600

    initial downloading function complete.  Working on threading now

--- follow-along/Chapters 10-20/threaded_download_xkcd.py
+++ follow-along/Chapters 10-20/threaded_download_xkcd.py
@@ -5,39 +5,34 @@ A multi-threaded version of the xkcd downloader
 from chapter 12 follow-along
 '''
 import requests, os, bs4
+import logging as log
 
-url = 'https://xkcd.com'
-os.chdir('follow-along/Chapters 10-20')
-os.makedirs('xkcd', exist_ok=True)
+log.basicConfig(level=log.DEBUG, format='%(asctime)s: %(message)s')
 
-i = 0
 
-#while not comic_url.endswith('#'): # original author's code, but didn't want to download this much
-while i < 3:
-    print(f'downloading page {url}...')
-    res = requests.get(url)
-    res.raise_for_status()
-
-    soup = bs4.BeautifulSoup(res.text, 'html.parser')
-    comic_element = soup.select('#comic > img')
-
-    if comic_element == []:
-        print('Could not find comic image.')
-    else:
-        comic_url = 'https:' + comic_element[0].get('src')
-        print(f'Downloading image {comic_url}...')
-        res = requests.get(comic_url)
+def download_comic(start_comic, end_comic):
+    for url_num in range(start_comic, end_comic):
+        print(f'Downloading page https://xkcd.com/{url_num}...')
+        res = requests.get(f'https://xkcd.com/{url_num}')
         res.raise_for_status()
 
-        image_file = os.path.join('xkcd', os.path.basename(comic_url))
-        
-        # changed up from the original follow-along to use the more readable "with" statement
-        with open(image_file, 'wb') as file:
-            for chunk in res.iter_content(100000):
-                file.write(chunk)
+        soup = bs4.BeautifulSoup(res.text, 'html.parser')
 
-    prev_link = soup.select('#middleContainer > ul:nth-child(4) > li:nth-child(2) > a')[0]
-    url = 'https://xkcd.com' + prev_link.get('href')
-    print(url)
-    
-    i += 1
+        comic_elem = soup.select('#comic img')
+        if comic_elem == []:
+            print('could not find image.')
+            return
+        else:
+            comic_url = 'https:' + comic_elem[0].get('src')
+            res = requests.get(comic_url)
+            res.raise_for_status()
+
+        file_name = os.path.join('xkcd_threaded', os.path.basename(comic_url))
+        with open(file_name, 'wb') as image_file:
+            for chunk in res.iter_content(100000):
+                image_file.write(chunk)
+
+
+os.chdir('follow-along/Chapters 10-20')
+os.makedirs('xkcd_threaded', exist_ok=True)
+download_comic(1000, 1001)
