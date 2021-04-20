bc9a6ee9985cb749429acc019b3ffac841bedc0f
austinare <austinjrichards@outlook.com>
2021-04-20 15:49:48 -0600

    got pretty printing working for live stopwatch updates

--- Chapter 17/prettified_stpowatch.py
+++ Chapter 17/prettified_stpowatch.py
@@ -9,7 +9,7 @@ data could be placed in an email or text file.
 '''
 import time
 import pyperclip
-import logging as log
+import pprint
 
 print('Press ENTER to begin, afterward, press ENTER to click the stopwatch.  Press Ctrl-C to quit')
 input()
@@ -19,18 +19,23 @@ start_time = time.time()
 last_time = start_time
 lap_num = 1
 
+data_list = []
+
 try:
     while True:
         input()
         lap_time = round(time.time() - last_time, 2)
         total_time = round(time.time() - start_time, 2)
 
-        '''
-        TODO: print lap number, total time, and lap time as seperate values? use str.ljust() and str.rjust()
-        print_string = f'Lap # {lap_num}: {total_time} ({lap_time})'
+        print_string = f'Lap # {lap_num}: {str(total_time).rjust(6)} ({str(lap_time).center(6)})'
         print(print_string, end='')
-        '''
+        data_list.append(print_string)
+
         lap_num += 1
-        last_time = time.time()
+        last_time = time.time()     
 except KeyboardInterrupt:
+    pretty_data = pprint.pformat(data_list)
+    print('\n')
+    print(pretty_data)
+    #TODO: change the data type from a list to a string with newlines between each item.
     print('\nDone.')
