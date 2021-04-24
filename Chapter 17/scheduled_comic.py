65803922b52b18ba7983f2793e954cc19fb43e4f
austinare <austinjrichards@outlook.com>
2021-04-24 16:06:45 -0600

    created function to identify if file already exists.  Downloads them if they don't already exist

--- Chapter 17/scheduled_comic.py
+++ Chapter 17/scheduled_comic.py
@@ -9,6 +9,7 @@ import os, requests, bs4
 from pathlib import Path
 import logging as log
 log.basicConfig(level=log.DEBUG, format='%(asctime)s: %(message)s')
+log.disable(log.CRITICAL)
 
 
 def get_all_paths(directory):
@@ -20,22 +21,42 @@ def get_all_paths(directory):
     return file_paths
 
 
-def download_new(page):
+def download_new():
     # get the page
+    page = 'http://www.lunarbaboon.com'
     res = requests.get(page)
     res.raise_for_status()
 
-    # parse for the first comic, get the title
+    # parse for the first comic, get the title and image link
     soup = bs4.BeautifulSoup(res.text, 'html.parser')
     first_comic = soup.select('div.journal-entry-text > h2 > a')[0]
     comic_title = first_comic.get_text()
+    comic_file = comic_title + '.png'
 
-    #TODO check if the comic name is on the desktop already
+    # check if the comic name is on the desktop already
+    # first, get files from the the desktop
     desk_path = os.path.abspath(os.path.join(os.environ['HOMEPATH'], 'OneDrive/Desktop'))
     file_names = [Path(path).stem for path in get_all_paths(desk_path) if path.endswith('.png')]
 
+    # if the file isn't present, download it
     if comic_title not in file_names:
-        #TODO save the file!
-        pass
+        # get the link
+        download_comic = soup.select('div.journal-entry-text > div.body > p:nth-child(1) > span > span > img')[0]
+        download_link = page + download_comic['src']
 
-download_new('http://www.lunarbaboon.com/')
+        # get the page
+        res = requests.get(download_link)
+        res.raise_for_status()
+
+        # download the image
+        filepath = os.path.join(desk_path, comic_file)
+        with open(filepath, 'wb') as file:
+            for chunk in res.iter_content(100000):
+                file.write(chunk)
+        print('New comic available!')
+
+    else:
+        print('No new comic')
+
+
+download_new()
