8cf2bcd8af099ea0ae719f9e69be335597fb08fa
austinare <austinjrichards@outlook.com>
2021-05-01 10:07:14 -0600

    completed text_me() function for notification programs!

--- Chapter 18/twilio_test.py
+++ Chapter 18/twilio_test.py
@@ -4,16 +4,27 @@ Austin Richards 4/29/21
 A program to test twilio out!
 '''
 import os
+from pathlib import Path
 from twilio.rest import Client
-import logging as log
-
-os.chdir('Chapter 18')
-log.basicConfig(level=log.DEBUG, format='%(asctime)s: %(message)s')
-
-# get credentials and cell numbers
-sid, auth_token, twilio_num, my_cell = open('twilio-creds.txt').read().splitlines()
 
 
-# send a message
-twi_client = Client(sid, auth_token)
-message = twi_client.messages.create(body='Hello!', from_=twilio_num, to=my_cell)
+def text_me(send_string):
+    '''
+    sends a text to my cell.  Must use double quotes for input,
+    otherwise twilio just won't send the text and won't throw 
+    any exceptions.
+    '''
+    # get credentials and cell numbers
+    sid, auth_token, twilio_num, my_cell = open('twilio-creds.txt').read().splitlines()
+
+    # send message
+    client = Client(sid, auth_token)
+    message = client.messages \
+                    .create(
+                        body=send_string,
+                        from_=twilio_num,
+                        to=my_cell
+                    )
+
+
+#text_me("Hello!!!")
