3f5999992e8857ff7fe77274e8876ced52dea599
austinare <austinjrichards@outlook.com>
2021-05-03 05:27:17 -0600

    completed record keeping to avoid repeat chores for users

--- Chapter 18/random_chore.py
+++ Chapter 18/random_chore.py
@@ -6,7 +6,9 @@ them a randomly assigned chore for the week.  This program also avoids
 repeating any chores that the person did the last week.
 '''
 import random
+import pprint
 import ezgmail as gmail
+import chore_record
 
 # initialize gmail
 gmail.init()
@@ -14,20 +16,26 @@ gmail.init()
 # chores, emails, and previous week's chores done
 chores = ['Dishes', 'Vacuum', 'Bathroom', 'Walk Dog']
 emails = {'Austin':'austinjrichards@outlook.com', 'Also Austin':'austin.pythonmagic@gmail.com'}
-#TODO: store prev_chore in a .txt file to keep a record of what each person has done.  This functionality
-# does not work with this format...
-prev_chore = {'Austin':'Vacuum', 'Also Austin':'Walk Dog'}
+print(chore_record.prev_chore)
 
 for name, email in emails.items():
     # assign chore, check they didn't do it last week, and update the list of what they did
     while True:
         rand_chore = random.choice(chores)
-        if rand_chore != prev_chore[name]:
-            prev_chore[name] = rand_chore
+        if rand_chore != chore_record.prev_chore[name]:
+            chore_record.prev_chore[name] = rand_chore
             chores.remove(rand_chore)
             break
 
     # send an email letting them know which chore to do this week
     subject = 'Weekly Chores!'
     body = f'Dear {name},\nYou have been assigned to the following chore this week: {rand_chore}\n\nCheers!'
+    print(f'Sending email to {name}. Chore: {rand_chore}')
     gmail.send(email, subject, body)
+
+# write changes to chore record
+data = pprint.pformat(chore_record.prev_chore)
+print(data)
+file = open('chore_record.py', 'w')
+file.write(f'prev_chore ={data}\n')
+file.close()
