f00f94d1a111fcc154f9a79fc261256a25d5621c
austinare <austinjrichards@outlook.com>
2021-05-04 15:11:48 -0600

    basic login to imap server complete

--- Chapter 18/email_unsubscriber.py
+++ Chapter 18/email_unsubscriber.py
@@ -7,10 +7,28 @@ If found, it opens all of the links in a webbrowser-
 allowing the user to click through and unsubscribe on 
 all of them.
 '''
-import bs4, imapclient, webbrowser
+import bs4
+import imaplib
+import webbrowser
+import pyinputplus as pyip
 
-#TODO: set up and log in to IMAP server
+def unsubscribe_all(mailbox='INBOX'):
+    #TODO: for each mail in the INBOX, search for "unsubscribe" - add link to list of links
 
-#TODO: for each mail in the INBOX, search for "unsubscribe" - add link to list of links
+    #TODO: for each link in the list, open it in the webbrowser
+    pass
 
-#TODO: for each link in the list, open it in the webbrowser
+
+# login info
+user = open('email_creds.txt').read()
+password = pyip.inputPassword('Input Password: ')
+folder = 'INBOX'
+
+# login to imap server, select folder
+email = imaplib.IMAP4_SSL('imap.gmail.com')
+try:
+    box = email.login(user, password)
+    print('login successful')
+except imaplib.IMAP4.error:
+    print('login failed')
+email.select(folder)
