2bad63562fc4350aae9fcd7062f2e527f3474c1a
austinare <austinjrichards@outlook.com>
2021-05-14 09:49:58 -0600

    added resize image function, main function.  Can now successfully add logos to the bottom of images.  Doesn't work with images that are already smaller than the logo.

--- Chapter 19/add_logo.py
+++ Chapter 19/add_logo.py
@@ -8,39 +8,55 @@ the corner of all image files in the specified folder.
 import os
 from PIL import Image
 
-os.chdir('Chapter 19')
-SQUARE_FIT_SIZE = 300
-LOGO_FILENAME = 'catlogo.png'
 
-logo_img = Image.open(LOGO_FILENAME)
-logo_width, logo_height = logo_img.size
-
-for filename in os.listdir('.'):
-    if not (filename.endswith('.png') or filename.endswith('.jpg')) \
-        or filename == LOGO_FILENAME:
-            continue
-    
-    print(f'Processing {filename}...')
+def resize_image(size, filename):
+    '''
+    resize_image takes a max size and image filename as arguments
+    and returns an image object that has resized the file.  This 
+    resize function preserves the aspect ratio.
+    '''
     im = Image.open(filename)
     width, height = im.size
 
-    #TODO make this resizing section into a function for later use
-    if width > SQUARE_FIT_SIZE and height > SQUARE_FIT_SIZE:
-        # calculate the new width and height to resize to
+    if width > size and height > size:
         if width > height:
-            height = int((SQUARE_FIT_SIZE / width) * height)
-            width = SQUARE_FIT_SIZE
+            height = int((size / width) * height)
+            width = size
         else:
-            width = int((SQUARE_FIT_SIZE / height) * width)
-            height = SQUARE_FIT_SIZE
+            width = int((size / height) * width)
+            height = size
 
         print(f'Resizing {filename}...')
-        im = im.resize((width, height))
+        resized_im = im.resize((width, height))
+        return resized_im
+    else:
+        return im
 
-    print(f'Adding logo to {filename}...')
-    #TODO figure out why this won't work.  Seems like the logo image is too large.
-    im.paste(logo_img, (width - logo_width, height - logo_height), logo_img)
 
-    os.makedirs('withLogo', exist_ok=True)
-    im.save(os.path.join('withLogo', filename))
-    print()
+def main():
+    SQUARE_FIT_SIZE = 300
+    LOGO_FILENAME = 'catlogo.png'
+    LOGO_SIZE = 10 # how many times smaller the logo will be than the image
+
+    logo_img = resize_image(int(SQUARE_FIT_SIZE / LOGO_SIZE), LOGO_FILENAME)
+    logo_width, logo_height = logo_img.size
+
+    for filename in os.listdir('.'):
+        if not (filename.endswith('.png') or filename.endswith('.jpg')) \
+            or filename == LOGO_FILENAME:
+                continue
+        
+        print(f'Processing {filename}...')
+        im = resize_image(SQUARE_FIT_SIZE, filename)
+        width, height = im.size
+
+        print(f'Adding logo to {filename}...')
+        #TODO figure out why this won't work.  Seems like the logo image is too large.
+        im.paste(logo_img, (width - logo_width, height - logo_height), logo_img)
+
+        os.makedirs('withLogo', exist_ok=True)
+        im.save(os.path.join('withLogo', filename))
+        print()
+
+os.chdir('Chapter 19')
+main()
