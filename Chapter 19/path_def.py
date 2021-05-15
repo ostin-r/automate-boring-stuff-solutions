7ad3b2c237cf19aa0f96e1f83be87018735c0e57
austinare <austinjrichards@outlook.com>
2021-05-15 14:51:31 -0600

    rename file

--- Chapter 19/path_def.py
+++ Chapter 19/path_def.py
@@ -8,30 +8,10 @@ files with a certain extension.
 import re, os, shutil
 
 def get_all_paths(directory):
-
     file_paths = []
-
     for path, dirs, files in os.walk(directory):
-
         for filename in files:
             filepath = os.path.join(path, filename)
             file_paths.append(filepath)
 
     return file_paths
-
-
-def select_copy(source, dest, ext):
-
-    source = os.path.abspath(source)
-    dest = os.path.abspath(dest)
-    
-    ext_regex = re.compile('\.{}$'.format(ext))
-    file_paths = get_all_paths(source)
-
-    for file in file_paths:
-        if ext_regex.search(file) is not None:
-            source = os.path.join(source, file)
-            shutil.copy(source, dest)
-
-
-select_copy('Chapter 9', '.', 'py')
