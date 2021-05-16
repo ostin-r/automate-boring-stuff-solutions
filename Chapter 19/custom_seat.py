7ee98a6bf860aebfa2e5f3df44e96dc0199edc6f
austinare <austinjrichards@outlook.com>
2021-05-16 13:40:49 -0600

    started necessary imports.  Created TODO list for program

---
+++ Chapter 19/custom_seat.py
@@ -0,0 +1,17 @@
+'''
+Austin Richards 5/16/21
+
+custom_seat.py will create custom printable images of
+party seating cards.  Each card will have the recipient's
+name, generated from guests.txt and will be sized 4in x 5in.
+'''
+import os
+from PIL import Image, ImageDraw, ImageFont
+
+#TODO: get the names, for each name:
+
+    #TODO: Add guests name to the invitation using an ImageDraw object
+
+    #TODO: Add the decorative art to the invitation (using paste method?)
+
+    #TODO: Save the image to a new folder
