a51729522ee6a791abe7114d986949d3a40a54c3
austinare <austinjrichards@outlook.com>
2021-05-27 08:51:49 -0600

    added form filling features for entire page in the form.  currently only testing with first user in the data set.

--- Chapter 20/form_filler.py
+++ Chapter 20/form_filler.py
@@ -5,11 +5,12 @@ form_filler.py opens a pre-set google form
 and automatically enters the data from form_data
 '''
 import pyautogui
+import webbrowser
 
 def main():
 
     formData = [{'name': 'Alice', 'fear': 'eavesdroppers', 'source': 'wand',
-            'robocop': 4, 'comments': 'Tell Bob I said hi.'},
+            'robocop': 1, 'comments': 'Tell Bob I said hi.'},
             {'name': 'Bob', 'fear': 'bees', 'source': 'amulet', 'robocop': 4,
             'comments': 'n/a'},
             {'name': 'Carol', 'fear': 'puppets', 'source': 'crystal ball',
@@ -22,29 +23,46 @@ def main():
 
     # initialization stuff
     pyautogui.PAUSE = 0.5 # this sets the universal pause between functions to half a second
-    print('Ensure that the browser window is active and the form is loaded.')
+    webbrowser.open('https://autbor.com/form', new=1)
+    submit_another_response = 'https://docs.google.com/forms/u/0/d/e/1FAIpQLScSVDFU76rZvbO_tiIwSt6d9sOK0CZyS9KKMCP6cP5O5W5lVQ/viewform?usp=form_confirm'
 
-    # TODO: give the user a chance to kill the process
-    print('>>> 5-SECOND PAUSE TO LET USER PRESS CTRL-C <<<')
-    pyautogui.sleep(5)
+    for person in formData:
+        # give the user a chance to kill the process, wait for webbrowser to load
+        print('>>> 5-SECOND PAUSE TO LET USER PRESS CTRL-C <<<')
+        pyautogui.sleep(5)
+        print(f"Entering {person['name']}'s info...")
 
-    # TODO: Wait until the form page has loaded.
+        # fill out the name and greatest fears field
+        pyautogui.write(['\t', '\t'])
+        pyautogui.write(person['name'] + '\t')
+        pyautogui.write(person['fear'] + '\t')
 
-    # TODO: Fill out the Name Field.
+        # fill out the Source of Wizard Powers field
+        if person['source'] == 'wand':
+            pyautogui.write(['down', 'enter'], 0.5)
+        elif person['source'] == 'amulet':
+            pyautogui.write(['down', 'down', 'enter'] , 0.5)
+        elif person['source'] == 'crystal ball':
+            pyautogui.write(['down', 'down', 'down', 'enter'] , 0.5)
+        elif person['source'] == 'money':
+            pyautogui.write(['down', 'down', 'down', 'down', 'enter'] , 0.5)
 
-    # TODO: Fill out the Greatest Fear(s) field.
+        # Fill out the RoboCop field.
+        pyautogui.write(['\t', ' '] + ['right'] * (person['robocop'] - 1))
 
-    # TODO: Fill out the Source of Wizard Powers field.
+        # Fill out the Additional Comments field
+        pyautogui.write('\t\t' + person['comments'] + '\t')
 
-    # TODO: Fill out the RoboCop field.
+        # click submit, wait 5 seconds
+        pyautogui.sleep(0.5)
+        pyautogui.press('enter')
+        print('Form submitted!')
+        pyautogui.sleep(5)
 
-    # TODO: Fill out the Additional Comments field.
-
-    # TODO: Click Submit.
+        break #TODO remove this line when done testing
 
     # TODO: Wait until form page has loaded.
 
     # TODO: Click the Submit another response link.
-    pass
 
 main()
