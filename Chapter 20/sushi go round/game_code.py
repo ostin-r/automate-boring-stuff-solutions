fa4e85ca87a641ee25eef5662f7b435a4273ca9d
austinare <austinjrichards@outlook.com>
2021-06-05 12:55:21 -0600

    completed game startup function

--- Chapter 20/sushi go round/game_code.py
+++ Chapter 20/sushi go round/game_code.py
@@ -4,6 +4,10 @@ main code for the sushi go round bot
 import webbrowser
 import pyautogui
 
+# globals ------
+x_pad = 227
+y_pad = 599
+# --------------
 
 def waitForImage(img_name):
     # pauses program until image is identified (for loading webpages)
@@ -11,15 +15,45 @@ def waitForImage(img_name):
         element = pyautogui.locateOnScreen(img_name)
         if element is not None: return
 
+def waitForPixel(pos, color):
+    # pauses program until pixel is identified
+    # position & color are both tuples of len 2 and 3 respectively
+    while True:
+        im = pyautogui.screenshot()
+        if im.getpixel(pos) == color: break
+
+def mousePos(cords):
+    # moves the mouse within the game play area
+    pyautogui.moveTo(x_pad + cords[0], y_pad + cords[1])
+
+def getCords():
+    x,y = pyautogui.position()
+    x = x - x_pad
+    y = y - y_pad
+    return [x, y]
 
 def startGame():
-    # open browser and wait for game to load
+    # open browser, wait for page to load
     webbrowser.open('https://www.miniclip.com/games/sushi-go-round/en/#')
     waitForImage('miniclip_img.PNG')
 
     # move the mouse then scroll
     pyautogui.moveTo(x=1000, y=1000) # moved mouse here because args not working for scroll method
     pyautogui.scroll(-410)
+
+    # click big play button
+    waitForPixel((929,940), (253,140,58))
+    pyautogui.click()
+
+    # click play button
+    waitForPixel((1226,756), (111,254,101))
+    pyautogui.sleep(0.5)
+    pyautogui.click()
+
+    # click both continue buttons
+    waitForPixel((887,1392), (255,179,246))
+    pyautogui.click(x=887, y=1392)
+    pyautogui.click()
 
 def main():
     startGame()
