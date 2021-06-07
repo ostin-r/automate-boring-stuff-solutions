a082900bafea5321da26417c10317508eaf0e7e1
austinare <austinjrichards@outlook.com>
2021-06-06 18:55:31 -0600

    refactored most of startGame() method to keep pixel references relative.  This will help preserve this code across different devices as long as x_pad and y_pad are initialized correctly.

--- Chapter 20/sushi go round/game_code.py
+++ Chapter 20/sushi go round/game_code.py
@@ -10,57 +10,69 @@ log.basicConfig(level=log.DEBUG, format='%(asctime)s: %(message)s')
 # globals ------
 x_pad = 227
 y_pad = 599
+# x_pad and y_pad are the top corner of the game play area
+# these can easily be changed if the computer the bot is being
+# run on is of a different resolution.
 # --------------
 
 
 def waitForImage(img_name):
-    # pauses program until image is identified (for loading webpages)
+    # pauses program until image is identified (good for loading webpages)
     while True:
         element = pyautogui.locateOnScreen(img_name)
         if element is not None: return
 
-def waitForPixel(pos, color):
-    # pauses program until pixel is identified
-    # position & color are both tuples of len 2 and 3 respectively
+
+def waitForPixel(pixel):
+    # pauses program until correct pixel is identified in play area
+    # pixel = [x, y, (R, G, B)]
+    pixel[0] += x_pad
+    pixel[1] += y_pad
     while True:
         im = pyautogui.screenshot()
-        if im.getpixel(pos) == color: break
+        if im.getpixel((pixel[0], pixel[1])) == pixel[2]: break
+
 
 def mousePos(cords):
     # moves the mouse within the game play area
     pyautogui.moveTo(x_pad + cords[0], y_pad + cords[1])
 
-def getCords():
-    x,y = pyautogui.position()
+
+def gamePixel():
+    '''
+    returns the color and relative game position of the cursor
+    '''
+    x, y = pyautogui.position()
+    im = pyautogui.screenshot()
+    color = im.getpixel((x, y))
+
     x = x - x_pad
     y = y - y_pad
-    return [x, y]
+    return [x, y, color]
+
 
 def startGame():
-    # open browser, wait for page to load
-    webbrowser.open('https://www.miniclip.com/games/sushi-go-round/en/#')
-    waitForImage('miniclip_img.PNG')
-
     # move the mouse then scroll
+    webbrowser.open('https://www.miniclip.com/games/sushi-go-round/en/#')
+    waitForImage('miniclip_menu.PNG')
     pyautogui.moveTo(x=1000, y=1000) # moved mouse here because args not working for scroll method
     pyautogui.scroll(-410)
 
-    # click big play button
-    waitForPixel((929,940), (253,140,58))
-    pyautogui.click()
-    log.debug('click: big play button')
+    # press big play button
+    # [709, 360, (253, 136, 58)]
 
-    # click play button
-    waitForPixel((1226,756), (111,254,101))
-    pyautogui.sleep(0.5)
-    pyautogui.click()
-    log.debug('click: play button')
+    # press little play button
+    # [779, 412, (75, 205, 245)]
 
-    # click both continue buttons
-    waitForPixel((887,1392), (255,179,246))
-    pyautogui.click(x=887, y=1392)
-    pyautogui.click()
-    log.debug('click: continue buttons')
+    # press the continue button
+    # [743, 776, (255, 45, 236)]
+
+    # press the skip button (skip tutorial)
+    # [1160, 902, (255, 203, 46)]
+
+    # press continue button again
+    # [629, 748, (255, 179, 246)]
+
 
 def main():
     startGame()
